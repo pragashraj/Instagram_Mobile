bee3004a11b728cc6d63d780f94ae7ed8acc4214
S.Pragashraj <rajpragashraj30@gmail.com>
2020-05-13 17:50:39 +0530

    post component & home screen

---
+++ src/components/Post.js
@@ -0,0 +1,146 @@
+import React, { Component } from 'react'
+import { Text, View , StyleSheet , Image , TouchableOpacity} from 'react-native'
+
+class Post extends Component {
+    render() {
+        return (
+            <View>
+                <View style={styles.postHeader}>
+                    <View style={styles.proImageBlock}>
+                        <Image source={require('../assets/icons/proImage.png')} style={styles.proImage}/>
+                    </View>
+
+                    <View style={styles.postHolderBlock}>
+                        <Text style={styles.postHolder}>postHolder</Text>
+                    </View>
+
+                    <View style={styles.moreBlock}>
+                        <TouchableOpacity>
+                            <Image source={require('../assets/icons/more.png')} style={styles.moreImage}/>
+                        </TouchableOpacity>
+                    </View>
+                       
+                </View>
+
+                <View style={styles.post}>
+                    <Image source={require('../assets/icons/post.jpg')} style={styles.postContent}/>
+                </View>
+
+                <View style={styles.userAction}>
+                    <TouchableOpacity>
+                        <Image source={require('../assets/icons/like.png')} style={styles.actions}/>
+                    </TouchableOpacity>
+
+                    <TouchableOpacity>
+                        <Image source={require('../assets/icons/comments.png')} style={styles.actions}/>
+                    </TouchableOpacity>
+
+                    <TouchableOpacity>
+                        <Image source={require('../assets/icons/message.png')} style={styles.actions}/>
+                    </TouchableOpacity>
+    
+                </View>
+
+                <View style={styles.likesCountBlock}>
+                    <Text style={styles.likesCount}>Liked by 123</Text>
+                </View>
+
+                <View style={styles.commentsBlock}>
+                    <Text style={styles.comments}>view comments .....</Text>
+                </View>
+
+            </View>
+        )
+    }
+}
+
+const styles=StyleSheet.create({
+
+    postHeader:{
+        width:'100%',
+        height:'10%',
+        flexDirection:'row',
+        alignItems:'center',
+        borderTopWidth:0.2,
+        backgroundColor:'lightgray',
+    },
+
+    proImageBlock:{
+        width:'15%',
+        height:'100%',
+        justifyContent:'center',
+        alignItems:'center'
+    },
+
+    proImage:{
+        width:'65%',
+        height:'90%',
+    },
+
+    postHolderBlock:{
+        width:'65%',
+        height:'100%',
+        justifyContent:'center',
+    },
+
+    postHolder:{
+        fontSize:17,
+        fontWeight:'bold'
+    },
+
+    moreBlock:{
+        width:'20%',
+        height:'100%',
+        justifyContent:'center',
+        alignItems:'center'
+    },
+
+    post:{
+        width:'100%',
+        height:'60%',
+    },
+
+    postContent:{
+        width:'100%',
+        height:'100%',
+    },
+
+    userAction:{
+        width:'100%',
+        height:'10%',
+        backgroundColor:'lightgray',
+        flexDirection:'row',
+        alignItems:'center',
+        paddingLeft:'2%'
+    },
+
+    actions:{
+        marginLeft:'5%'
+    },
+
+    likesCountBlock:{
+        width:'100%',
+        height:'10%',
+        justifyContent:'center'
+    },
+
+    likesCount:{
+        marginLeft:'4%',
+        fontSize:16,
+    },
+
+
+    commentsBlock:{
+        width:'100%',
+        height:'10%',
+        backgroundColor:'yellow',
+        justifyContent:'center'
+    },
+
+    comments:{
+        marginLeft:'4%',
+        fontSize:16,
+    }
+})
+
+export default Post
--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,12 +1,87 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { View, Text , StyleSheet , Image , TouchableOpacity} from 'react-native'
+
+import Post from '../components/Post'
 
 const HomeScreen = () => {
     return (
         <View>
-            <Text>Home</Text>
+            <View style={styles.header}>
+                <View style={styles.camera}>
+                    <TouchableOpacity>
+                        <Image source={require('../assets/icons/camera.png')}/>
+                    </TouchableOpacity>
+                </View>
+
+                <View style={styles.titleBlock}>
+                    <Text style={styles.title}>Instagram</Text>
+                </View>
+
+                <View style={styles.message}>
+                    <TouchableOpacity>
+                        <Image source={require('../assets/icons/message.png')}/>
+                    </TouchableOpacity>
+                </View>
+            </View>
+
+            <View style={styles.stories}>
+                <TouchableOpacity style={styles.addBlock}>
+                    <Image source={require('../assets/icons/add.png')} style={styles.add}/>
+                </TouchableOpacity>
+            </View>
+            <View style={styles.posts}>
+                <Post/>
+            </View>
         </View>
     )
 }
+
+
+const styles=StyleSheet.create({
+    header:{
+        width:'100%',
+        height:'10%',
+        flexDirection:'row',
+        borderBottomWidth:0.2,
+        backgroundColor:'lightgray'
+    },
+
+    camera:{
+        width:'15%',
+        height:'100%',
+        justifyContent:'center',
+        alignItems:'center'
+    },
+
+    titleBlock:{
+        width:'70%',
+        height:'100%',
+        justifyContent:'center',
+        alignItems:'center'
+    },
+
+    title:{
+        fontFamily:'Precious',
+        fontSize:35,
+    },
+
+    message:{
+        width:'15%',
+        height:'100%',
+        justifyContent:'center',
+        alignItems:'center'
+    },
+
+
+    stories:{
+        width:'100%',
+        height:'17%',
+        justifyContent:'center'
+    },
+    posts:{
+        width:'100%',
+        height:'73%',
+    }
+})
 
 export default HomeScreen
